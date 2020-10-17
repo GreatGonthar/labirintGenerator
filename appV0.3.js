8c426b31ba9575af0f6ba2d83da28d0fdb31af2a
GreatGonthar <gontharforeverrules@gmail.com>
2020-10-17 19:39:58 +1000

    Create appV0.3.js

---
+++ appV0.3.js
@@ -0,0 +1,6 @@
+let canvas = document.createElement("canvas");
+let ctx = canvas.getContext("2d");
+canvas.width = 960;
+canvas.height = 720;
+canvas.style = 'background:silver';
+document.body.appendChild(canvas);
